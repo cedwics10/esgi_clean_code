9937331673a6df7795d90e8a7ddf062f8753f9a9
= <brochettes40@outlook.fr>
2025-10-30 15:28:34 +0100

    Testing values for 3, 4, 5 doubles

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const NUMBER_OF_DICE = 5;
+
+const SCORE_BY_OCCURENCES = { 3: 28, 4: 35, 5: 50 };
+
 function computeTotalScoreOf(setOfthrows) {
-    return 50;
+    
+    const histogram = {};
+
+    for (let i = 0; i < NUMBER_OF_DICE; i++) {
+        const diceValue = setOfthrows[i];
+        histogram[diceValue] = (histogram[diceValue] || 0) + 1;
+    }
+
+    const maxOccurrences = Math.max(...Object.values(histogram));
+
+    return SCORE_BY_OCCURENCES[maxOccurrences] ?? 50;
 }
 
-export { computeTotalScoreOf }
+export { computeTotalScoreOf };
--- index.test.js
+++ index.test.js
@@ -3,11 +3,13 @@ import { computeTotalScoreOf } from '.';
 
 const INITIAL_VALUE = 0;
 
-describe('Test yams with only one basiC throw', () => {
-  it.each([
-    { uniqueThrow: [1, 1, 1, 1, 1], expected: 50 },
-  ])('Sum of $uniqueThrow equals $expected', ({ uniqueThrow, expected }) => {
-    let total = computeTotalScoreOf(uniqueThrow);
-    expect(total).toBe(expected);
-  });
+describe('Values depending on number of doubles', () => {
+    it.each([
+        { uniqueThrow: [1, 1, 1, 1, 1], expected: 50 },
+        { uniqueThrow: [1, 1, 1, 1, 2], expected: 35 },
+        { uniqueThrow: [1, 1, 1, 2, 2], expected: 28 }
+    ])('Sum of $uniqueThrow equals $expected', ({ uniqueThrow, expected }) => {
+        let total = computeTotalScoreOf(uniqueThrow);
+        expect(total).toBe(expected);
+    });
 });
